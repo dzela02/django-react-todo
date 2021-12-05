633550846e2aa58023476bcbfeab5634adc01478
marko <dzelanovi@gmail.com>
2021-12-05 14:55:29 +0100

    Implemented Material UI

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@material-ui/core";
+
+const Header = () => {
+  return (
+    <div>
+      <Typography variant="h3" align="center">
+        TODO App
+      </Typography>
+    </div>
+  );
+};
+
+export default Header;
